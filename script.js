a4a969de8cbcbf347ab0448b71ee801a1d946976
B BHARATH KUMAR REDDY <bharathboienipalli@gmail.com>
2025-06-18 07:49:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+function mapLetters(word) {
+  const result = {};
+
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i];
+    if (result.hasOwnProperty(letter)) {
+      result[letter].push(i);
+    } else {
+      result[letter] = [i];
+    }
+  }
+
+  return result;
+}
+
+console.log(mapLetters("dodo"));    
+console.log(mapLetters("froggy"));  
+console.log(mapLetters("grapes"));  
